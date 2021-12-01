5645938bcf45a875c03a9a509421310abb59b089
Iago Martínez <iago@fundapps.co>
2021-12-01 13:23:19 +0000

    Fix relative file path problem

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,9 +1,12 @@
 import unittest
 from context import day1
+from pathlib import Path
+
+THIS_DIR = Path(__file__).parent
 
 class TestDay1(unittest.TestCase):
     def test_day1(self):
-        depths = day1.readdepths('../data/day1_sample.txt')            
+        depths = day1.readdepths(THIS_DIR.parent / 'data/day1_sample.txt')            
         self.assertEqual(10, len(depths))
 
     def test_countdepthincreases(self):
@@ -11,5 +14,5 @@ class TestDay1(unittest.TestCase):
         self.assertEqual(1, day1.countdepthincreases(depths))
         
     def test_sampledepths(self):
-        depths = day1.readdepths('../data/day1_sample.txt')            
+        depths = day1.readdepths(THIS_DIR.parent /'data/day1_sample.txt')            
         self.assertEqual(7, day1.countdepthincreases(depths))
